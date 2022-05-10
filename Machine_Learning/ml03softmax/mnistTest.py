fa5218c76c2b9de2dec31bdebe73f737c4aa3cab
gju0828 <97085193+gju0828@users.noreply.github.com>
2022-05-10 17:35:18 +0900

    05/10

---
+++ Machine_Learning/ml03softmax/mnistTest.py
@@ -0,0 +1,10 @@
+from pandas import Series
+import matplotlib.pyplot as plt
+
+data = [0.9218, 0.926, 0.9737, 0.9752]
+myseries = Series(data)
+myseries.plot(kind='bar')
+
+filename = 'mnist_test_result.png'
+plt.savefig(filename)
+print(filename + ' 저장 완료')
